457809ccb53f2c553bc769620232ffefce9901db
lourw <56288712+lourw@users.noreply.github.com>
2025-01-09 09:22:04 -0800

    chore(status-check): change tag

--- apps/status-check/src/app/page.tsx
+++ apps/status-check/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome status-check 👋
+              Welcome, status-check 👋
             </h1>
           </div>
 
